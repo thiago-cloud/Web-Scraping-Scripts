82f1b29bd02e7e95eb7e6cd5de7754ca9e2f1ad1
thiago-cloud <thiago.154ti@gmail.com>
2025-04-25 21:53:24 -0300

    extração e exportação de notícias do G1 para Excel

--- autoNews/autoNews.py
+++ autoNews/autoNews.py
@@ -14,3 +14,30 @@ site = BeautifulSoup(content, 'html.parser')
 # Encontrando os blocos de notícias
 # HTML da notícia
 noticias = site.find_all('div', attrs={'class': 'feed-post-body'})
+
+# Extraindo informações de cada notícia
+for noticia in noticias:
+  # Título
+  titulo = noticia.find('a', attrs={'class': 'feed-post-link'})
+
+  # print(titulo.text)
+  # print(titulo['href']) # link da notícia
+
+  # Subtítulo: div class="feed-post-body-resumo"
+  subtitulo = noticia.find('div', attrs={'class': 'feed-post-body-resumo'})
+
+  if (subtitulo):
+    # print(subtitulo.text)
+    lista_noticias.append([titulo.text, subtitulo.text, titulo['href']])
+  else:
+    lista_noticias.append([titulo.text, '', titulo['href']])
+
+
+# Criando um DataFrame e exportando
+news = pd.DataFrame(lista_noticias, columns=['Título', 'Subtítulo', 'Link'])
+
+# Salvando no execel
+# Para funcionar e necessário instalar o openpyxl
+news.to_excel('noticias.xlsx', index=False)
+
+# print(news)
