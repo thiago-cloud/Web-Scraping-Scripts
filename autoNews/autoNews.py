4c38260171a8d7755cd973e4045497a8bc6021e2
thiago-cloud <thiago.154ti@gmail.com>
2025-04-25 21:38:21 -0300

    coleta inicial de notícias do G1 com BeautifulSoup

--- autoNews/autoNews.py
+++ autoNews/autoNews.py
@@ -1,1 +1,16 @@
-print("teste git")
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+lista_noticias = []
+
+# Acessando o site
+response = requests.get('https://g1.globo.com/')
+
+content = response.content
+
+site = BeautifulSoup(content, 'html.parser')
+
+# Encontrando os blocos de notícias
+# HTML da notícia
+noticias = site.find_all('div', attrs={'class': 'feed-post-body'})
