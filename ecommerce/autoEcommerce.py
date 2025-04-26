81ca6609ab1b7422e3be5273f1d719ad2161c76a
thiago-cloud <thiago.154ti@gmail.com>
2025-04-25 22:17:07 -0300

    implementa busca e coleta de produtos no Mercado Livre com BeautifulSoup

--- ecommerce/autoEcommerce.py
+++ ecommerce/autoEcommerce.py
@@ -1,1 +1,16 @@
-print("teste git")
+
+# > EXEMPLO
+# - Obtendo produtos do Mercado Livre a partir de uma busca realizada pelo usuário
+
+import requests
+from bs4 import BeautifulSoup
+
+url_base = 'https://lista.mercadolivre.com.br/'
+
+produto_nome = input('Qual produto você deseja? ')
+
+response = requests.get(url_base + produto_nome)
+
+site = BeautifulSoup(response.text, 'html.parser')
+
+produtos = site.find_all('div', attrs={'class': 'ui-search-result__wrapper'})
